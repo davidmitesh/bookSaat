52dea9bd61825c57fc4fcc7373f28122abd9e32d
cruzelx <cruze.alex@gmail.com>
2019-11-30 15:59:57 +0545

    Final recommendation added

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const _=require('lodash');
-const express=require('express');
+const _ = require('lodash');
+const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-const path=require('path');
+const path = require('path');
 var url = require('url');
 const fs = require('fs');
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 let cors = require('cors');
 const Books = require("./model/books");
+
 mongoose.connect(
     "mongodb://localhost:27017/bookSaat",
     { useNewUrlParser: true, useUnifiedTopology: true },
@@ -18,102 +19,110 @@ mongoose.connect(
 );
 
 const port = 8080;
-var bodyParser= require('body-parser');
+var bodyParser = require('body-parser');
 
-let app=express();
+let app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
-<<<<<<< HEAD
-const pythonProcess = spawn('python',["./recommendation_Data.py"]);
-
-pythonProcess.stdout.on('data', (data) => {
-    console.log(data);
-});
-app.use(express.json());
-=======
->>>>>>> b1457460f71e7421af5503e3de37f652224d8a9d
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     const spawn = require("child_process").spawn;
 
-    const pythonProcess = spawn('python',["./recommendationSystem/recommendation_Data.py"]);
+    const pythonProcess = spawn('python', ["./recommendationSystem/recommendation_Data.py"]);
 
     pythonProcess.stdout.on('data')
-})
-app.get("/recommended", async (req, res)=>{ 
-    const book = await Books.find({}, (err, docs) => { 
-        if (err) return;
-        console.log(docs);
-        res.json(docs);
-    });
+});
+
+let recommend = [];
+Books.find({}, (err, docs) => {
+    if (err) return;
+    recommend = docs;
+});
+
+
+app.get("/recommended", async (req, res) => {
+    res.send(recommend);
+});
+
+let science_data = [];
+let drama_data = [];
+let mix_data = [];
+
+Books.find({ "category.catName": "Science" }, (err, docs) => {
+    science_data = docs;
+    // console.log(science_data);
+});
+
+Books.find({ "category.catName": "Fiction" }, (err, docs) => {
+    drama_data = docs;
+    // console.log(drama_data);
 });
 
 
 //function module that will be calling recommendation --collaborative filtering
-<<<<<<< HEAD
-app.get('/search', (req, res) => {
-    console.log(req.query.q);
-    res.send(req.query.q);
-    
-});
-=======
-app.get('/recommended',(req,res)=>{
+app.get('/recommended', (req, res) => {
     const spawn = require("child_process").spawn;
 
-    const pythonProcess = spawn('python',["./recommendationSystem/recommendation_Data.py"]);
+    const pythonProcess = spawn('python', ["./recommendationSystem/recommendation_Data.py"]);
 
     pythonProcess.stdout.on('data')
-})
-let c_universe=false;
-let c_mistakes=false;
-let c_mix=false;
-app.get('/search',(req,res)=>{
-   console.log(req.query.q);
-   let a = _.includes(req.query.q,'universe');
-   let b= _.includes(req.query.q,'revolution');
+});
 
-   if (a==true){
-       !c_universe;
-   }
-   if(b==true){
-       !c_mistakes;
-   }
-   if ( c_universe && c_mistakes ){
-       !c_mix
-   }
+app.get('/exchange', (req, res) => {
+    let a = _.includes(req.query.q.toLowerCase(), 'stephen'); 
+    if (a) { 
+        res.send(science_data);
+    }
+});
 
-   if (c_universe){
-       res.send(science_data);
-   }
-   if (c_mistakes){
-       res.send(drama_data);
-   }
-   if(c_mix){
-       res.send(mix_data);
-   }
+let c_universe = false;
+let c_mistakes = false;
+let c_mix = false;
+app.get('/search', (req, res) => {
+    console.log(req.query.q);
+    let a = _.includes(req.query.q, 'universe');
+    let b = _.includes(req.query.q, 'revolution');
+    console.log(a);
+
+    if (a == true) {
+        recommend = science_data;
+        res.send(science_data);
+        
+
+    }
+    if (b == true) {
+        recommend = drama_data;
+        res.send(drama_data);
+    }
+    if (c_universe && c_mistakes) {
+        !c_mix
+    }
+
+    if (c_universe) {
+        res.send(science_data);
+    }
+    if (c_mistakes) {
+        res.send(drama_data);
+    }
+    if (c_mix) {
+        mix_data.push(science_data);
+        mix_data.push(drama_data);
+        console.log(mix_data);
+        res.send(mix_data);
+    }
+
+});
 
 
-
-
-
-
-
-   console.log(a);
-
-
-
-
->>>>>>> b1457460f71e7421af5503e3de37f652224d8a9d
-
-app.get('/authId', async (req, res) => { 
+app.get('/authId', async (req, res) => {
     const authId = await Books.find({ authId: req.query.q }, (err, docs) => {
         if (err) return;
-        console.log(docs);
+        // console.log(docs);
         res.json(docs);
     });
 });
 
-app.listen(port,()=>{
-    console.log('server is high on port ',port);
- })
+app.listen(port, () => {
+    console.log('server is high on port ', port);
+});
