732d0cd36423db8c40fb28b5c9aa3920377535bc
cruzelx <cruze.alex@gmail.com>
2019-11-30 14:43:42 +0545

    add database

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const http = require('http');
 const WebSocket = require('ws');
 const path=require('path');
 var url = require('url');
-const fs=require('fs');
-let cors=require('cors');
-const port=3000;
+const fs = require('fs');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+let cors = require('cors');
+mongoose.connect(
+    "mongodb://localhost:27017/bookSaat",
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log("Connected to DB");
+    }
+);
+
+const port=process.env.PORT;
 var bodyParser= require('body-parser');
 
 let app=express();
@@ -23,11 +33,21 @@ pythonProcess.stdout.on('data', (data) => {
 app.get('/',(req,res)=>{
     res.send('Hey,I am called');
 })
+app.get("/recommended", async (req, res)=>{ 
+    const book = await Books.find({}, (err, docs) => { 
+        if (err) return;
+        console.log(docs);
+        res.json(docs);
+    });
+});
 
 //function module that will be calling recommendation --collaborative filtering
-app.get('/get_recommendation',(req,res)=>{
-
-})
+app.get('/search', (req, res) => {
+    console.log("alex");
+    // const data = req.body;
+    // res.send(data);
+    // //console.log(data);
+});
 
 app.listen(port,()=>{
     console.log('server is high on port ',port);
