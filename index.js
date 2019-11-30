dbb3019803b84970e2caedf52262c8e000a31d53
davidmitesh <excelrock_mitesh@yahoo.com>
2019-11-30 10:42:37 +0545

    InitialFilteringcode_dataset

---
+++ index.js
@@ -0,0 +1,8 @@
+const _=require('lodash');
+const express=require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path=require('path');
+var url = require('url');
+const fs=require('fs');
+let cors=require('cors');
---
+++ recommendation_Data.py
@@ -0,0 +1,6 @@
+dataset = {
+    "Mitesh": {"Romance": 1, "Science": 1, "Thriller": 1, "Action": 1, "Emotional": 1,},
+    "Alex": {"Romance": 1, "Science": 1, "Thriller": 1, "Action": 1, "Emotional": 1,},
+    "Bibek": {"Romance": 1, "Science": 1, "Thriller": 1, "Action": 1, "Emotional": 1,},
+    "Kiran": {"Romance": 1, "Science": 1, "Thriller": 1, "Action": 1, "Emotional": 1,},
+}
